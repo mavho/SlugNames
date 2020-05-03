5da6f6097b360fa15172ced2369909e2f6f6ab80
Eric Huang <erlhuang@ucsc.edu>
2020-05-03 01:32:08 -0700

    some more stuff

--- SlugNames/routes.py
+++ SlugNames/routes.py
@@ -5,5 +5,11 @@ from SlugNames.namesGenerator import generateNames
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    generateNames()
+    thenames = generatename
     return render_template('index.html')
+
+@app.route('/game', methods=['GET'])
+def theGame():
+    thenames = generateNames()
+    #thenames is a 5x5 2d list with the words for codenames 
+    return render_template('thegame.html', thenames=thenames)
