6ead2225022ebcc3a31619335a6ad561cb870cd5
Eric Huang <erlhuang@ucsc.edu>
2020-05-02 17:53:32 -0700

    some basic shit

--- SlugNames/routes.py
+++ SlugNames/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template 
 from SlugNames import app
+from SlugNames.namesGenerator import generateNames
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
+    generateNames()
     return render_template('index.html')
