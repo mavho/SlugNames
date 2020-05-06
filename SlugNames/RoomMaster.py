71a2ce09c44436e7bd9fc78646fe31c24e8df054
mavHo <homaverick@gmail.com>
2020-05-05 17:51:39 -0700

    moved html files into respective includes layout folder, added RoomMaster class, moved generate board into RM, added link to button

---
+++ SlugNames/RoomMaster.py
@@ -0,0 +1,72 @@
+import random 
+import sys
+
+
+
+class RoomMaster():
+    def __init__(self):
+        """
+        Class to manage the state of a game board.
+        """
+        ### We made word board and stateboard two different functions
+        ### This way we can alter either if desired.
+        ### TODO: Move this out to config file.
+        self.word_list = ['Darrel Long', 'Mackey', 'CE12', 'Dining Hall', 'Flexi Dollars', \
+            'Iveta', 'Owl\'s Nest', 'Raymond\'s Catering', 'CMPS-12B', 'Tracy Larrabee', \
+            'Yerba', 'Science and Engineering Library', 'Strike', 'Patrick Tantalo', 'Asbestos', \
+            'Porter', 'College Nine/Ten', 'Stevenson', 'Crown', 'Seshadhri', \
+            'Dimitris', 'Slug', 'Deer', 'International', 'Hello Kitty', \
+            'Gaming', '4/20', 'Marijuana', 'Psychidelics', 'Bus', \
+            'Housing', 'Drunk Monkeys', 'Opers', 'Walk', 'Classroom Unit 2']
+        self.red_agent_count = 8
+        self.blue_agent_count = 8
+        self.double_agent_count = 1 
+        self.assasin_count = 1
+        self.innocent_bystanders_count = 7
+        self.word_board = self._generateWordBoard() 
+        self.state_board = self._generateStateBoard()
+
+
+    def changeWordBoard(self):
+        """
+        Sets current GM word_board to a new board
+        """
+        self.word_board = self._generateWordBoard()
+    def changeWordBoard(self):
+        """
+        Sets current GM state_board to a new board
+        """
+        self.state_board= self._generateStateBoard()
+
+    def _generateWordBoard(self):
+        """
+        Places words onto word_board. Alters word_board.
+        """
+        random.shuffle(self.word_list)
+        rows, cols = (5, 5) 
+        arr2D = [[0 for i in range(cols)] for j in range(rows)] 
+        #its a list of 25 words, turning it into a 2D list with 5 list of 5 words
+        for i in range(5):
+            for n in range(5):
+                arr2D[i][n] = self.word_list[i*5 + n]
+        return arr2D
+    def _generateStateBoard(self):
+        """
+        Places agents and bystanders across a board
+        R: Red, B: BLUE, D:double, A: Assasin, I: bystander
+        """
+        hold = ['A','D']
+        for r in range(8):
+            hold.append('R')
+        for r in range(8):
+            hold.append('B')
+        for r in range(7):
+            hold.append('I')
+        random.shuffle(hold)
+        res = [[0] * (5) for i in range(5)]
+        for i in range(5):
+            for n in range(5):
+                res[i][n] = hold[i*5 + n]
+        return res
+
+
