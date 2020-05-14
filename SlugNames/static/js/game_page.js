76c66bb3775f867cfece78e7139b6d4ee43f8d07
mavHo <homaverick@gmail.com>
2020-05-13 23:08:55 -0700

    dummy page

---
+++ SlugNames/static/js/game_page.js
@@ -0,0 +1,8 @@
+/*
+Not sure what to name this page. Regardless this file should handle any elements, really the DOM of the
+game UI
+
+It also will obtain info and send info to the webservice
+*/
+
+console.log("You're in the game page!");
