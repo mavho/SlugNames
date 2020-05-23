b3ae12d794cb78b099756e0bdd081ba42290582b
Eric Huang <erlhuang@ucsc.edu>
2020-05-23 16:33:12 -0700

    spymasters are shown the state board

--- SlugNames/static/js/game_page.js
+++ SlugNames/static/js/game_page.js
@@ -7,7 +7,6 @@ It also will obtain info and send info to the webservice
 
 var board_len = 5;
 var board_width = 5;
-
 console.log(user_name + ' ' + team + ' ' + role);
 console.log(roomid);
 $(document).ready(function() {
@@ -16,13 +15,17 @@ $(document).ready(function() {
     for(let r=0; r < board_len; r++){
         for(let c= 0; c<board_width; c++){
             thewords[(r*board_len)+c].addEventListener("click", bindClick(r,c));
+            thewords[(r*board_len)+c].setAttribute("id", r + '-' + c);
         }
     }
  
     function bindClick(row, col) {
         return function() {
-            console.log("row" + row + " :column " + col);
-            socket.emit("flip card", {'row': row, 'col':col, 'roomid': roomid, 'username':user_name});
+            if(role != 'spymaster'){
+                console.log("row" + row + " :column " + col);
+                socket.emit("flip card", {'row': row, 'col':col, 'roomid': roomid, 'username':user_name});
+                $('#' + row + '-' + c).append('clicked');
+            }
         };
     }
 });
